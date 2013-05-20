da05141b7f4197899703eb9e49a7b278cb7df928
DennisMackayFisher@gmail.com <DennisMackayFisher@gmail.com@33abdffd-855b-6b4e-dc28-f59f20e8dda7>
2013-05-20 11:37:37 +0000

    git-svn-id: http://pvbeancounter.googlecode.com/svn/trunk_v2@1365 33abdffd-855b-6b4e-dc28-f59f20e8dda7

---
+++ DeviceControl/DeviveManager_EW4009.cs
@@ -0,0 +1,206 @@
+﻿/*
+* Copyright (c) 2013 Dennis Mackay-Fisher
+*
+* This file is part of PV Bean Counter
+* 
+* PV Bean Counter is free software: you can redistribute it and/or 
+* modify it under the terms of the GNU General Public License version 3 or later 
+* as published by the Free Software Foundation.
+* 
+* PV Bean Counter is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with PV Bean Counter.
+* If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Threading;
+using System.IO;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Linq;
+using System.Text;
+using PVSettings;
+using MackayFisher.Utilities;
+using Conversations;
+using DeviceStream;
+using DeviceDataRecorders;
+using GenThreadManagement;
+using PVBCInterfaces;
+using Algorithms;
+using Device;
+
+namespace DeviceControl
+{
+
+    public class DeviceManager_EW4009 : DeviceManager_PassiveController<EW4009_Device, CC128_LiveRecord, CC128_HistoryRecord, CC128ManagerParams>
+    {
+        // max allowed difference between CC Meter Time and computer time in Minutes
+        public const int MeterTimeSyncTolerance = 10;
+        // Time sync difference that triggers warnings
+        public const int MeterTimeSyncWarning = 5;
+        private DateTime? MeterTimeSyncWarningTime = null;
+        // Is current CC Meter Time in Sync with computer time
+        private bool MeterTimeInSync = false;
+
+        //private int DbInterval;
+        private DateTime? LastRecordTime = null;
+        private CC128ManagerParams ManagerParams;
+
+        private CompositeAlgorithm_EW4009 DeviceAlgorithm;
+        private int DatabaseInterval;
+
+        public DeviceManager_EW4009(GenThreadManager genThreadManager, DeviceManagerSettings mmSettings,
+            IDeviceManagerManager imm)
+            : base(genThreadManager, mmSettings, imm)
+        {
+            AlgorithmParams aParams;
+            aParams.Protocol = Protocol;
+            aParams.EndianConverter16Bit = Protocol.EndianConverter16Bit;
+            aParams.EndianConverter32Bit = Protocol.EndianConverter32Bit;
+            mmSettings.CheckListenerDeviceId();
+            aParams.BlockList = mmSettings.ListenerDeviceSettings.BlockList;
+            aParams.AlgorithmList = mmSettings.ListenerDeviceSettings.AlgorithmList;
+            aParams.DeviceName = mmSettings.ListenerDeviceSettings.Description;
+            aParams.ErrorLogger = ErrorLogger;
+            DeviceAlgorithm = new CompositeAlgorithm_EW4009(aParams);
+
+            DatabaseInterval = mmSettings.DBIntervalInt;
+        }
+
+        public override void Initialise()
+        {
+            base.Initialise();
+        }
+
+        protected override void LoadParams()
+        {
+            ManagerParams = new CC128ManagerParams();
+            ManagerParams.DeviceType = PVSettings.DeviceType.EnergyMeter;
+            ManagerParams.RecordingInterval = DeviceManagerSettings.DBIntervalInt;
+            ManagerParams.QueryInterval = DeviceManagerSettings.MessageIntervalInt;
+            ManagerParams.HistoryHours = DeviceManagerSettings.HistoryHours.Value;
+        }
+
+        protected void LogMessage(String routine, String message, LogEntryType logEntryType = LogEntryType.DetailTrace)
+        {
+            GlobalSettings.LogMessage(routine, message, logEntryType);
+        }
+
+        protected override EW4009_Device NewDevice(DeviceManagerDeviceSettings dmDevice)
+        {
+            return new EW4009_Device(this, dmDevice);
+        }
+
+        public override bool DoWork()
+        {
+            int index;
+
+            DateTime lastZero = DateTime.MinValue;
+
+            if (ReadingInfo.RecordsAvailEvent.WaitOne(10000))
+            {
+                ReadingInfo.RecordsAvailEvent.Reset();
+
+                for (index = 0; index < DeviceList.Count; index++)
+                {
+                    while (ReadingInfo.LiveRecords[index].Count > 0)
+                    {
+                        try
+                        {
+                            ProcessOneLiveRecord(DeviceList[index], ReadingInfo.LiveRecords[index][0]);
+                        }
+                        catch (Exception e)
+                        {
+                            LogMessage("DoWork.ProcessOneLiveRecord - Exception: " + e.Message, LogEntryType.ErrorMessage);
+                            // discard record causing error - attempt to continue
+                        }
+
+                        ReadingInfo.RecordsMutex.WaitOne();
+                        ReadingInfo.LiveRecords[index].RemoveAt(0);
+                        ReadingInfo.RecordsMutex.ReleaseMutex();
+                    }
+                }
+
+                for (index = 0; index < DeviceList.Count; index++)
+                {
+                    while (ReadingInfo.HistoryRecords[index].Count > 0)
+                    {
+                        try
+                        {
+                            EW4009_Device device = DeviceList[index];
+                            if (device.DeviceSettings.UseHistory)
+                                ProcessOneHistoryRecord(device, ReadingInfo.HistoryRecords[index][0]);
+                        }
+                        catch (Exception e)
+                        {
+                            LogMessage("DoWork.ProcessOneHistoryRecord - Exception: " + e.Message, LogEntryType.ErrorMessage);
+                            // discard record causing error - attempt to continue
+                        }
+
+                        ReadingInfo.RecordsMutex.WaitOne();
+                        ReadingInfo.HistoryRecords[index].RemoveAt(0);
+                        ReadingInfo.RecordsMutex.ReleaseMutex();
+                    }
+
+                }
+            }
+
+            return true;
+        }
+
+        protected void ProcessOneLiveRecord(MeterDevice<CC128_LiveRecord, CC128_HistoryRecord, CC128EnergyParams> device, CC128_LiveRecord liveRecord)
+        {
+            // DMF *******
+            // Remove the following when not using DEBUG
+            // Debug causes a rush of records less than a millisecond apart when execution is paused for multiple "intervals"
+#if DEBUG
+            liveRecord.TimeStampe = liveRecord.MeterTime;
+#endif
+            // DMF *******
+
+            bool curSyncStatus = MeterTimeInSync;
+            int timeError = Convert.ToInt32(Math.Abs((liveRecord.MeterTime - liveRecord.TimeStampe).TotalMinutes));
+            MeterTimeInSync = timeError <= MeterTimeSyncTolerance;
+
+            // Issue warnings every hour if above warning threshold but below max tolerance
+            if (MeterTimeInSync)
+                if (timeError >= MeterTimeSyncWarning)
+                {
+                    if (MeterTimeSyncWarningTime == null || (MeterTimeSyncWarningTime + TimeSpan.FromHours(1.0)) < DateTime.Now)
+                    {
+                        LogMessage("ProcessOneRecord", "Meter time variance at WARNING threshold: " + timeError +
+                        " minutes - History update is unreliable", LogEntryType.Information);
+                        MeterTimeSyncWarningTime = DateTime.Now;
+                    }
+                }
+                else
+                    MeterTimeSyncWarningTime = null;
+
+            // Log transitions across max tolerance threshold
+            if (MeterTimeInSync != curSyncStatus)
+                if (MeterTimeInSync)
+                    LogMessage("ProcessOneRecord", "Meter time variance within tolerance: " + timeError +
+                        " minutes - History adjust available", LogEntryType.Information);
+                else
+                    LogMessage("ProcessOneRecord", "Meter time variance exceeds tolerance: " + timeError +
+                        " minutes - History adjust disabled", LogEntryType.Information);
+
+            DateTime curTime = DateTime.Now;
+            bool dbWrite = (LastRecordTime == null
+                || DeviceBase.IntervalCompare(ManagerParams.RecordingInterval, LastRecordTime.Value, curTime) != 0);
+
+            device.ProcessOneLiveReading(liveRecord);
+        }
+
+        protected void ProcessOneHistoryRecord(MeterDevice<CC128_LiveRecord, CC128_HistoryRecord, CC128EnergyParams> device, CC128_HistoryRecord histRecord)
+        {
+            if (MeterTimeInSync && device.DeviceManagerDeviceSettings.UpdateHistory)
+                device.ProcessOneHistoryReading(histRecord);
+        }
+    }
+}
---
+++ DeviceControl/EW4009_Device.cs
@@ -0,0 +1,261 @@
+﻿/*
+* Copyright (c) 2012 Dennis Mackay-Fisher
+*
+* This file is part of PV Bean Counter
+* 
+* PV Bean Counter is free software: you can redistribute it and/or 
+* modify it under the terms of the GNU General Public License version 3 or later 
+* as published by the Free Software Foundation.
+* 
+* PV Bean Counter is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with PV Bean Counter.
+* If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PVBCInterfaces;
+using PVSettings;
+using Algorithms;
+using DeviceDataRecorders;
+using MackayFisher.Utilities;
+
+namespace Device
+{
+
+    public class EW4009_Device : MeterDevice<CC128_LiveRecord, CC128_HistoryRecord, CC128EnergyParams>
+    {
+        public FeatureSettings Feature_EnergyAC { get; protected set; }
+
+        private Double EstEnergy = 0.0;
+
+        public EW4009_Device(DeviceControl.DeviceManager_EW4009 deviceManager, DeviceManagerDeviceSettings deviceSettings)
+            : base(deviceManager, deviceSettings, "Watts Clever", "EW4009", "")
+        {
+            DeviceParams = new CC128EnergyParams();
+            DeviceParams.QueryInterval = deviceSettings.QueryIntervalInt;
+            DeviceParams.RecordingInterval = deviceSettings.DBIntervalInt;
+
+            DeviceParams.CalibrationFactor = deviceSettings.CalibrationFactor;
+            Feature_EnergyAC = deviceSettings.DeviceSettings.GetFeatureSettings(FeatureType.EnergyAC, deviceSettings.Feature);
+        }
+
+        protected override DeviceDetailPeriodsBase CreateNewPeriods(FeatureSettings featureSettings)
+        {
+            return new DeviceDetailPeriods_EnergyMeter(this, featureSettings, PeriodType.Day, TimeSpan.FromTicks(0));
+        }
+
+        public override bool ProcessOneLiveReading(CC128_LiveRecord liveReading)
+        {
+            if (FaultDetected)
+                return false;
+
+            bool res = false;
+
+            int? minPower = null;
+            int? maxPower = null;
+
+            DeviceDetailPeriods_EnergyMeter days = null;
+
+            String stage = "Identity";
+            try
+            {
+                stage = "Reading";
+
+                DateTime curTime = DateTime.Now;
+                //bool dbWrite = (LastRecordTime == null
+                //    || DeviceInfo.IntervalCompare(DatabaseInterval, LastRecordTime.Value, curTime) != 0);
+
+                TimeSpan duration;
+                int power = liveReading.Watts >= DeviceManagerDeviceSettings.ZeroThreshold ? liveReading.Watts : 0;
+                try
+                {
+                    duration = EstimateEnergy((double)power, curTime, 6.0F);
+                }
+                catch (Exception e)
+                {
+                    LogMessage("ProcessOneLiveReading - Error calculating EstimateEnergy - probably no PowerAC retrieved - Exception: " + e.Message, LogEntryType.ErrorMessage);
+                    return false;
+                }
+
+                if (maxPower.HasValue)
+                {
+                    if (maxPower.Value < power)
+                        maxPower = power;
+                }
+                else
+                    maxPower = power;
+
+                if (minPower.HasValue)
+                {
+                    if (minPower.Value > power)
+                        minPower = power;
+                }
+                else
+                    minPower = power;
+
+                days = (DeviceDetailPeriods_EnergyMeter)FindOrCreateFeaturePeriods(Feature_EnergyAC.FeatureType, Feature_EnergyAC.FeatureId);
+
+                EnergyReading reading = new EnergyReading();
+                //liveReading.TimeStampe = DeviceBase.NormaliseReadingTime(liveReading.TimeStampe);
+
+                if (LastRecordTime.HasValue)
+                    reading.Initialise(days, liveReading.TimeStampe, LastRecordTime.Value, false);
+                else
+                    reading.Initialise(days, liveReading.TimeStampe, TimeSpan.FromSeconds(DeviceInterval), false);
+
+                LastRecordTime = liveReading.TimeStampe;
+
+                reading.EnergyCalibrationFactor = DeviceManagerDeviceSettings.CalibrationFactor;
+                reading.EnergyToday = null;
+                reading.EnergyTotal = null;
+                reading.Power = power;
+                reading.MinPower = minPower;
+                reading.MaxPower = maxPower;
+                reading.Mode = null;
+                reading.Volts = null;
+                reading.Amps = null;
+                reading.Frequency = null;
+                reading.Temperature = (double)liveReading.Temperature;
+                reading.ErrorCode = null;
+                reading.EnergyDelta = EstEnergy; // EstEnergy is an accumulation from the contributing 6 sec power values
+                //if (DeviceManagerDeviceSettings.CalibrationFactor != 1.0F)
+                //    reading.CalibrationDelta = reading.CalibrateableReadingDelta * DeviceManagerDeviceSettings.CalibrationFactor;
+
+                EstEnergy = 0.0;
+                minPower = null;
+                maxPower = null;
+
+                if (GlobalSettings.SystemServices.LogTrace)
+                    LogMessage("ProcessOneLiveReading - Reading - Time: " + liveReading.TimeStampe + " - Duration: " + (int)reading.Duration.TotalSeconds + " - EnergyToday: " + reading.EnergyToday
+                        + " - EnergyTotal: " + reading.EnergyTotal
+                        + " - EstEnergy: " + EstEnergy
+                        + " - Power: " + reading.Power
+                        + " - Mode: " + reading.Mode
+                        + " - FreqAC: " + reading.Frequency
+                        + " - Volts: " + reading.Volts
+                        + " - Current: " + reading.Amps
+                        + " - Temperature: " + reading.Temperature
+                        , LogEntryType.Trace);
+
+                stage = "record";
+
+                days.AddRawReading(reading);
+
+                if (IsNewdatabaseInterval(reading.ReadingEnd))
+                {
+                    days.UpdateDatabase(null, reading.ReadingEnd, false, PreviousDatabaseIntervalEnd);
+                    stage = "consolidate";
+                    List<OutputReadyNotification> notificationList = new List<OutputReadyNotification>();
+                    BuildOutputReadyFeatureList(notificationList, FeatureType.EnergyAC, 0, reading.ReadingEnd);
+                    UpdateConsolidations(notificationList);
+                }
+
+                if (EmitEvents)
+                {
+                    stage = "energy";
+                    EnergyEventStatus status = FindFeatureStatus(FeatureType.EnergyAC, 0);
+                    status.SetEventReading(curTime, 0.0, power, (int)duration.TotalSeconds, true);
+                    DeviceManager.ManagerManager.EnergyEvents.ScanForEvents();
+                }
+
+                stage = "errors";
+            }
+            catch (Exception e)
+            {
+                LogMessage("ProcessOneLiveReading - Stage: " + stage + " - Time: " + liveReading.TimeStampe + " - Exception: " + e.Message, LogEntryType.ErrorMessage);
+                return false;
+            }
+
+            return res;
+        }
+
+        // curTime is supplied if duration is to be calculated on the fly (live readings)
+        // curTime is null if duration is from a history record - standardDuration contains the correct duration
+        private DateTime? LastEstTime = null;
+        private TimeSpan EstimateEnergy(Double powerWatts, DateTime curTime, float standardDuration)
+        {
+            TimeSpan duration;
+            if (LastEstTime.HasValue)
+                duration = (curTime - LastEstTime.Value);
+            else
+                duration = TimeSpan.FromSeconds(standardDuration);
+            LastEstTime = curTime;
+
+            Double newEnergy = (powerWatts * duration.TotalHours) / 1000.0; // watts to KWH
+            EstEnergy += newEnergy;
+
+            if (GlobalSettings.SystemServices.LogTrace)
+                GlobalSettings.SystemServices.LogMessage("EstimateEnergy", "Time: " + curTime + " - Power: " + powerWatts +
+                    " - Duration: " + duration.TotalSeconds + " - Energy: " + newEnergy, LogEntryType.Trace);
+
+            return duration;
+        }
+
+        public override bool ProcessOneHistoryReading(CC128_HistoryRecord histReading)
+        {
+            String stage = "Initial";
+            try
+            {
+                DeviceDetailPeriods_EnergyMeter days = (DeviceDetailPeriods_EnergyMeter)FindOrCreateFeaturePeriods(Feature_EnergyAC.FeatureType, Feature_EnergyAC.FeatureId);
+                DeviceDetailPeriod_EnergyMeter day;
+                // detect end of day history entry - applies to previous day
+                EnergyReading hist;
+                if (histReading.Time.TimeOfDay.TotalHours == 1.0) // 1:00am reading (duration 2 hours) crosses a period boundary
+                {
+                    stage = "Cross Period Boundary";
+                    if (GlobalSettings.SystemServices.LogTrace)
+                        GlobalSettings.SystemServices.LogMessage("ProcessOneHistoryReading", stage + " - Time: " + histReading.Time
+                            + " - Duration: " + histReading.Duration + " - Energy: " + histReading.Energy, LogEntryType.Trace);
+                    histReading.Time = histReading.Time.Date; // end of previous day - midnight
+                    histReading.Duration = histReading.Duration / 2; // halve duration and energy readings
+                    histReading.Energy = histReading.Energy / 2.0;
+                    if (histReading.Calculated.HasValue)
+                        histReading.Calculated = histReading.Calculated.Value / 2.0;
+
+                    stage = "Previous Day";
+                    if (GlobalSettings.SystemServices.LogTrace)
+                        GlobalSettings.SystemServices.LogMessage("ProcessOneHistoryReading", stage + " - Time: " + histReading.Time
+                            + " - Duration: " + histReading.Duration + " - Energy: " + histReading.Energy, LogEntryType.Trace);
+                    day = days.FindOrCreate(histReading.Time.AddDays(-1.0));  // midnight reading applies to previous day
+                    hist = new EnergyReading(days, histReading.Time, TimeSpan.FromSeconds(histReading.Duration));
+                    hist.EnergyDelta = histReading.Energy;
+                    hist.Temperature = histReading.Temperature;
+                    day.AdjustFromHistory(hist);
+                    histReading.Time = histReading.Time.AddHours(1.0); // reset time to 1:00am
+                    stage = "Start Day";
+                }
+                else
+                    stage = "Normal";
+
+                if (GlobalSettings.SystemServices.LogTrace)
+                    GlobalSettings.SystemServices.LogMessage("ProcessOneHistoryReading", stage + " - Time: " + histReading.Time
+                        + " - Duration: " + histReading.Duration + " - Energy: " + histReading.Energy, LogEntryType.Trace);
+
+                day = days.FindOrCreate(histReading.Time.Date); // get correct day
+                hist = new EnergyReading(days, histReading.Time, TimeSpan.FromSeconds(histReading.Duration));
+                hist.EnergyDelta = histReading.Energy;
+                hist.Temperature = histReading.Temperature;
+                day.AdjustFromHistory(hist);
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                LogMessage("ProcessOneHistoryReading - Stage: " + stage + " - Exception: " + e.Message, LogEntryType.ErrorMessage);
+                return false;
+            }
+        }
+
+        public override void SplitReadingSub(ReadingBase oldReading, DateTime splitTime, ReadingBase newReading1, ReadingBase newReading2)
+        {
+        }
+    }
+}
